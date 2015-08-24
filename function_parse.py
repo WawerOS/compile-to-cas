c79d8438fc3e3b58f3d91dc9a697653cea46c86e
WawerOS <lion@waposta.com>
2015-08-24 14:36:43 -0500

    major logic implemented

--- function_parse.py
+++ function_parse.py
@@ -17,8 +17,9 @@ def format_inverse_ops(parseable):
             parseable[i] = dictionary_bi_op[parseable[i]]
     return parseable
 
+
 def format_multiplication(parseable):
-    for i in range(len(parseable))-1:
+    for i in range(len(parseable) - 1):
         is_var_or_num = lambda x: x.isNumber or len(x) == 1
         if is_var_or_num(parseable[i]) and is_var_or_num(parseable[i + 1]):
             parseable.insert(i + 1, "*")
@@ -26,3 +27,26 @@ def format_multiplication(parseable):
 
 
 def format_trig(parseable):
+    for i in range(len(parseable)):
+        if parseable[i] in trig_list:
+            span = first_parantheses(parseable, i)
+            input_ = parseable[i+2:span]
+            if parseable[i] == trig_list[0]:
+                template = ["sin", "(", ")", "/", "cos", "(", ")"]
+                adjusted_once = insert_list(template, input_, 2)
+                adjusted_twice = insert_list(adjusted_once, input_, len(adjusted_once)-1)
+                del parseable[i:span+1]
+                insert_list(parseable, adjusted_twice,i + 1)
+
+def insert_list(parseable, insertable, i):
+    for j in range(len(insertable)):
+        parseable.insert(i,insertable[j])
+        i += 1
+    return parseable
+
+def first_parantheses(parseable, i):
+    char = parseable[i]
+    while char != ")":
+        i++
+        char = parseable[i]
+    return i
